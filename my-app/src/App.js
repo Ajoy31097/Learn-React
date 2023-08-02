bf6f32963506f65eddf8179526f483b4e82c0dfa
Ajoy31097 <123059362+Ajoy31097@users.noreply.github.com>
2023-08-02 22:21:47 +0530

    map function used to iterate through expenses without manually implementing them

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
 
-  const expense = [
+  const expenses = [
     { title: 'Food', amount: 50, location: 'Restaurant', date: new Date(2023, 2, 28) },
     { title: 'Petrol', amount: 100, location: 'Petrol Pump', date: new Date(2023, 4, 18) },
     { title: 'Movie', amount: 200, location: 'PVR', date: new Date(2023, 5, 21) },
@@ -16,9 +16,14 @@ function App() {
   return (
     <div>
       <h1>Expense Items</h1>
-      <ExpenseItem title={expense[0].title} amount={expense[0].amount} location={expense[0].location} date={expense[0].date}></ExpenseItem>
-      <ExpenseItem title={expense[1].title} amount={expense[1].amount} location={expense[1].location} date={expense[1].date}></ExpenseItem>
-      <ExpenseItem title={expense[2].title} amount={expense[2].amount} location={expense[2].location} date={expense[2].date}></ExpenseItem>
+      {expenses.map((expense, index) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          location={expense.location}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
