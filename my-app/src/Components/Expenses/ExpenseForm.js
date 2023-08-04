283b0966347b46f8f6cf090f44873d1298b5a170
Ajoy31097 <123059362+Ajoy31097@users.noreply.github.com>
2023-08-04 13:41:47 +0530

    Add Expense button activated for listening the form with data console.log

--- my-app/src/Components/Expenses/ExpenseForm.js
+++ my-app/src/Components/Expenses/ExpenseForm.js
@@ -1,49 +1,78 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css';
 
-const ExpenseForm = (props) =>{
+const ExpenseForm = (props) => {
 
-    const [enteredTitle, setenteredTitle] = useState('');
+    const [UserInput, setUserInput] = useState({
+        enteredTitle: '',
+        enteredAmount: '',
+        enteredDate: '',
+        enteredLocation: ''
+    });
+
     const titleChangeHandler = (event) => {
-        setenteredTitle(event.target.value);
+        setUserInput((prevState) => {
+            return { ...UserInput, enteredTitle: event.target.value }
+        });
     }
-    const [enteredAmount, setenteredAmount] = useState('');
     const amountChangeHandler = (event) => {
-        setenteredAmount(event.target.value);
+        setUserInput((prevState) => {
+            return { ...UserInput, enteredAmount: event.target.value }
+        });
     }
-    const [enteredDate, setenteredDate] = useState('');
     const dateChangeHandler = (event) => {
-        setenteredDate(event.target.value);
+        setUserInput((prevState) => {
+            return { ...UserInput, enteredDate: event.target.value }
+        });
     }
-    const [enteredLocation, setenteredLocation] = useState('');
     const locationChangeHandler = (event) => {
-        setenteredLocation(event.target.value);
+        setUserInput((prevState) => {
+            return { ...UserInput, enteredLocation: event.target.value }
+        });
     }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        const expenseData = {
+            title: UserInput.enteredTitle,
+            amount: UserInput.enteredAmount,
+            date: new Date(UserInput.enteredDate),
+            location: UserInput.enteredLocation
+        };
+        console.log(expenseData);
+        setUserInput({
+            enteredTitle: '',
+            enteredAmount: '',
+            enteredDate: '',
+            enteredLocation: ''
+        });
+    };
+
     return <div className='new-expense'>
-    <form>
-        <div className='new-expense__controls'>
-            <div className='new-expense__control'>
-            <label>Title</label>
-            <input type='text' onChange={titleChangeHandler}></input>
+        <form onSubmit={submitHandler}>
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <label>Title</label>
+                    <input type='text' value={UserInput.enteredTitle} onChange={titleChangeHandler}></input>
+                </div>
+                <div className='new-expense__control'>
+                    <label>Amount</label>
+                    <input type='number' min="0.01" step="0.01" value={UserInput.enteredAmount} onChange={amountChangeHandler}></input>
+                </div>
+                <div className='new-expense__control'>
+                    <label>Date</label>
+                    <input type='date' min="2022-01-01" max="2023-12-31" value={UserInput.enteredDate} onChange={dateChangeHandler}></input>
+                </div>
+                <div className='new-expense__control'>
+                    <label>Location</label>
+                    <input type='text' value={UserInput.enteredLocation} onChange={locationChangeHandler}></input>
+                </div>
             </div>
-            <div className='new-expense__control'>
-            <label>Amount</label>
-            <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}></input>
+            <div className='new-expense__actions'>
+                <button type='submit'>Add Expense</button>
             </div>
-            <div className='new-expense__control'>
-            <label>Date</label>
-            <input type='date' min="2022-01-01" max="2023-12-31" onChange={dateChangeHandler}></input>
-            </div>
-            <div className='new-expense__control' onChange={locationChangeHandler}>
-            <label>Location</label>
-            <input type='text'></input>
-            </div>
-        </div>
-        <div className='new-expense__actions'>
-        <button type='submit'>Add Expense</button>
-        </div>
-    </form>
-    </div> 
+        </form>
+    </div>
 }
 
 export default ExpenseForm;
