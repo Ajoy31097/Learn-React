f9bb337396e32775f20b89f5e52d59a573ae6662
Ajoy31097 <123059362+Ajoy31097@users.noreply.github.com>
2023-08-09 16:41:34 +0530

    Chart Implemented

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ExpenseItem from "./Components/Expenses/ExpenseItem";
 import ExpenseForm from "./Components/Expenses/ExpenseForm";
 import ExpensesFilter from "./Components/Expenses/ExpensesFilter";
+import ExpensesChart from "./Components/Expenses/ExpensesChart";
 
 function App() {
   const [expenses, setExpenses] = useState([]);
@@ -29,27 +30,29 @@ function App() {
       <h1>Expense Items</h1>
       <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
       <ExpensesFilter
-        selected={filteredYear}
-        onChangeFilter={filterChangeHandler}
+  selected={filteredYear}
+  onChangeFilter={filterChangeHandler}
+/>
+<ExpensesChart expenses={filteredExpenses} />
+{filteredExpenses.length === 0 ? (
+  <p>No Expenses Found</p>
+) : (
+  <>
+    {filteredExpenses.length === 1 ? (
+      <p>Only a single Expense here. Please add more...</p>
+    ) : null}
+    {filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        location={expense.location}
+        date={expense.date}
       />
-      {filteredExpenses.length === 0 ? (
-        <p>No Expenses Found</p>
-      ) : (
-        <>
-          {filteredExpenses.length === 1 ? (
-            <p>Only a single Expense here. Please add more...</p>
-          ) : null}
-          {filteredExpenses.map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              location={expense.location}
-              date={expense.date}
-            />
-          ))}
-        </>
-      )}
+    ))}
+  </>
+)}
+
     </div>
   ); 
           } 
--- my-app/src/Components/Expenses/ExpensesFilter.js
+++ my-app/src/Components/Expenses/ExpensesFilter.js
@@ -6,17 +6,17 @@ const ExpensesFilter = (props) => {
         props.onChangeFilter(event.target.value);
     };
 
-    return(
+    return (
         <div className="expenses-filter">
             <div className="expenses-filter__control">
-            <label>Filter by year</label>
-            <select value={props.selected} onChange={dropDownChangeHandler}>
-                <option value='2023'>2023</option>
-                <option value='2022'>2022</option>
-                <option value='2021'>2021</option>
-                <option value='2020'>2020</option>
-                <option value='2019'>2019</option>
-            </select>
+                <label>Filter by year</label>
+                <select value={props.selected} onChange={dropDownChangeHandler}>
+                    <option value='2023'>2023</option>
+                    <option value='2022'>2022</option>
+                    <option value='2021'>2021</option>
+                    <option value='2020'>2020</option>
+                    <option value='2019'>2019</option>
+                </select>
             </div>
         </div>
     )
